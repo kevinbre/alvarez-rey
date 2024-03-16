a975103062b605b2905da5b4a9e0313c21877903
kevinbre <b.kevin@hotmail.es>
2024-03-16 19:05:15 -0300

    refactor: changed project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import {Hero} from "./pages/hero";
+import {Hero} from "./components/hero/hero";
+import {Footer} from "./components/layout/footer/footer";
+import {Navbar} from "./components/layout/navbar/navbar";
 
 function App() {
     return (
         <>
+            <Navbar />
             <Hero />
+            <Footer />
         </>
     );
 }
---
+++ src/components/layout/navbar/navbar.tsx
@@ -0,0 +1,7 @@
+export function Navbar() {
+    return (
+        <>
+            <h1>hola boys!</h1>
+        </>
+    );
+}
