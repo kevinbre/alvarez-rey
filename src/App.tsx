7a9a74dc33fb221be2a68bffbb107add7922f877
SantiCombina <santicombina@gmail.com>
2024-03-18 14:18:23 -0300

    feature: Added Whatsapp button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import {Hero} from "./components/hero/hero";
 import {Footer} from "./components/layout/footer/footer";
 import {Navbar} from "./components/layout/navbar/navbar";
+import {WspButton} from "./components/ui/wsp-button";
 
 function App() {
     return (
-        <>
-            <Navbar />
-            <Hero />
-            <Footer />
-        </>
+        <div className="font-monserrat bg-neutral-950">
+            <Hero heroRef={heroRef} />
+            <Navbar footerRef={footerRef} heroRef={heroRef} />
+            <Footer footerRef={footerRef} />
+            <WspButton />
+        </div>
     );
 }
 
