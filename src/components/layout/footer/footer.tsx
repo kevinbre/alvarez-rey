e4c894d5c6b3c457a1339ed59918399ae5484045
GinoNovello <ginonovello9@gmail.com>
2024-03-18 20:08:47 -0300

    feature: Footer mobile

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -9,10 +9,10 @@ export function Footer({footerRef}: Props) {
 
     return (
         <footer ref={footerRef} className="container w-full bg-neutral-950">
-            <div className="mx-auto w-full max-w-screen-xl">
-                <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-3">
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-white uppercase ">Compañía</h2>
+            <div className="mx-auto max-w-screen-xl">
+                <div className="px-4 py-6 flex-col text-center md:grid gap-8 lg:py-8 md:grid-cols-3 flex items-center justify-between">
+                    <div className="mb-8 md:w-1/3">
+                        <h2 className="mb-6 text-sm font-semibold text-white uppercase">Compañía</h2>
                         <ul className="text-gray-300 dark:text-gray-400 font-medium">
                             <li className="mb-4">
                                 <a className={`${hovStyle}`} href="#">
@@ -31,9 +31,9 @@ export function Footer({footerRef}: Props) {
                             </li>
                         </ul>
                     </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-white uppercase ">Centro de ayuda</h2>
-                        <ul className="text-gray-300  font-medium">
+                    <div className="mb-8 md:w-1/3">
+                        <h2 className="mb-6 text-sm font-semibold text-white uppercase">Centro de ayuda</h2>
+                        <ul className="text-gray-300 font-medium">
                             <li className="mb-4">
                                 <a className={`${hovStyle}`} href="#">
                                     Whatsapp
@@ -56,9 +56,9 @@ export function Footer({footerRef}: Props) {
                             </li>
                         </ul>
                     </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-white uppercase ">Legal</h2>
-                        <ul className="text-gray-300  font-medium">
+                    <div className="md:w-1/3">
+                        <h2 className="mb-6 text-sm font-semibold text-white uppercase">Legal</h2>
+                        <ul className="text-gray-300 font-medium">
                             <li className="mb-4">
                                 <a className={`${hovStyle}`} href="#">
                                     Política de privacidad
@@ -77,9 +77,12 @@ export function Footer({footerRef}: Props) {
                         </ul>
                     </div>
                 </div>
-                <div className="px-4 py-6 bg-neutral-950 md:flex md:items-center md:justify-between">
-                    <span className="text-sm text-gray-300  sm:text-center">
-                        © 2024 <a>Estudio jurídico Alvarez Rey.</a>
+                <div className="px-4 py-6 bg-neutral-950 flex justify-center">
+                    <span className="text-sm text-gray-300">
+                        © 2024{" "}
+                        <a className="text-gray-300 hover:text-gray-100" href="#">
+                            Estudio jurídico Alvarez Rey.
+                        </a>
                     </span>
                 </div>
             </div>
